1868df0e56f144a60702c6671df4f9f2152e91b5
Brian Tilford <github@bgt.fastmail.com>
2019-04-24 20:35:20 -0400

    Fixed small problem with last data entry, and added first attempt at area and volume column code.

--- Analysis.R
+++ Analysis.R
@@ -1,2 +1,4 @@
 Pond_Data <- read.csv("CSV_DataStorage.csv", stringsAsFactors = FALSE)
 Pond_Data$Date <- as.Date( Pond_Data$Date )
+Pond_Data$Area <- ((Pond_Data$South + Pond_Data$North + 1500) / 2) * ((Pond_Data$West + Pond_Data$East + 1100) / 2) * pi
+Pond_Data$Volume <- (((Pond_Data$South + Pond_Data$North + 1500) / 2) * ((Pond_Data$West + Pond_Data$East + 1100) / 2) * (Pond_Data$Depth) * (4/3) * pi) / 2
